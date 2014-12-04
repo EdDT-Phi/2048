077e19824b3fd070d32bc2dba7b8fd0dc612d1d4
Eddie Tribaldos <eddt@cs.utexas.edu>
2014-12-03 22:35:25 -0600

    testing system

--- AI.java
+++ AI.java
@@ -3,9 +3,12 @@ public class AI{
 
 		game2048 game = new game2048();
 		int max = 0;
-		int i = 0;
+		int min = 1000000;
+		int sum = 0;
 
-		while (max < 2048) {
+		boolean test = true;
+
+		for (int i = 0; i < 100000; i++){
 			boolean moved;
 			while(game.movesPossible()) {
 				moved = game.moveDown();
@@ -20,10 +23,12 @@ public class AI{
 			}
 
 			max = Math.max(max, game.getMax());
+			min = Math.min(min, game.getMax());
+			sum += game.getMax();
 			game.newGame();
-			i++;
 		}
 		System.out.println("Max: " + max);
-		System.out.println("Tries: " + i);
+		System.out.println("Min: " + min);
+		System.out.println("Average: " + sum / 100000);
 	}
 }
